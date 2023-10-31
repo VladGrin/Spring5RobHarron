09c3c2fcc21b848123218d17db609de86d5e07fb
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2023-11-01 00:15:44 +0200

    Update chapter 4

---
+++ Chapter4/src/main/java/org/example/factory/AccessingFactoryBeans.java
@@ -0,0 +1,22 @@
+package org.example.factory;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.security.MessageDigest;
+
+public class AccessingFactoryBeans {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:./spring/app-context-xml.xml");
+        ctx.refresh();
+        ctx.getBean("shaDigest", MessageDigest.class);
+        MessageDigestFactoryBean factoryBean = (MessageDigestFactoryBean) ctx.getBean("&shaDigest");
+        try {
+            MessageDigest shaDigest = factoryBean.getObject();
+            System.out.println(shaDigest.digest("Hello world".getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            ctx.close();
+        }
+    }
+}

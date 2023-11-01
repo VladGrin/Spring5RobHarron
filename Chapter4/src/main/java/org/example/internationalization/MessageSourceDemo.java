f8e91145f93af15928ebf97147252546dca0e857
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2023-11-02 00:08:20 +0200

    Update chapter 4 add internalization

---
+++ Chapter4/src/main/java/org/example/internationalization/MessageSourceDemo.java
@@ -0,0 +1,20 @@
+package org.example.internationalization;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Locale;
+
+public class MessageSourceDemo {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:./spring/app-context-xml1.xml");
+        ctx.refresh();
+        Locale english = Locale.ENGLISH;
+        Locale german = new Locale("de", "DE");
+        System.out.println(ctx.getMessage("msg", null, english));
+        System.out.println(ctx.getMessage("msg", null, german));
+        System.out.println(ctx.getMessage("nameMsg", new Object[]{"John", "Mayer"}, english));
+        System.out.println(ctx.getMessage("nameMsg", new Object[]{"John", "Mayer"}, german));
+        ctx.close();
+    }
+}

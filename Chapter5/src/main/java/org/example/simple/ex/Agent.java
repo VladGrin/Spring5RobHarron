a7bcecd07fd9a7626dc2a55cc0a0da89ba73f602
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2023-11-04 21:28:07 +0200

    add chapter 5 AOP

---
+++ Chapter5/src/main/java/org/example/simple/ex/Agent.java
@@ -0,0 +1,12 @@
+package org.example.simple.ex;
+
+public class Agent {
+
+    public void speak() {
+        System.out.print("Bond");
+    }
+
+    public void run() {
+        System.out.print(" run");
+    }
+}
